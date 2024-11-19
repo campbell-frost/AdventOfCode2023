ce5267068f48a06f83739ea9af8718a8dc6da587
campbell-frost <campbellsfrost@gmail.com>
2024-11-18 19:13:50 -0500

    Day 1

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+// Day 1
+
 const input = [
   "1abc2",
   "pqr3stu8vwx",
@@ -5,8 +7,26 @@ const input = [
   "treb7uchet"
 ];
 
-((input: string[]) => {
-  input.forEach((str) => {
-    console.log(str);
+type ResultItem = {
+  one: number;
+  two: number;
+}
+
+let result: ResultItem[] = [];
+
+const findResult = (input: string[]) => {
+  input.forEach(str => {
+    result.push({
+      one: parseInt(str.split("").find(c => !isNaN(parseInt(c))) || '0'),
+      two: parseInt(str.split("").reverse().find(c => !isNaN(parseInt(c))) || '0'),
+    });
   });
-})(input);
+};
+
+findResult(input);
+
+let sum = 0;
+
+result.map(item => `${item.one}${item.two}`).forEach(item => sum += parseInt(item));
+
+console.log(sum);
